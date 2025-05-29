3b4a1c5d3d584899830c6a5ce2fef1672babe4b5
itsdrvgo <thedragoluca@gmail.com>
2025-05-29 19:08:37 +0530

    fix: change sourcemap option from 'linked' to 'inline'

--- server/build.ts
+++ server/build.ts
@@ -2,7 +2,7 @@ await Bun.build({
     entrypoints: ["./src/app.ts"],
     outdir: "./build",
     target: "bun",
-    sourcemap: "linked",
+    sourcemap: "inline",
     minify: true,
     external: ["axios", "express", "zod"],
     root: "./src",
